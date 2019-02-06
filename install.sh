5b39e51c60d508a482791fb8ab54a7811921340a
BNMetrics <luna@bnmetrics.com>
2019-02-06 09:52:33 +0000

    changed link to our own fork

--- install.sh
+++ install.sh
@@ -110,7 +110,7 @@ tmpdir=$(mktemp -d)
 
 cd "$tmpdir"
 
-git clone -b "$RELEASE" https://github.com/widdix/aws-ec2-ssh.git
+git clone -b "$RELEASE" https://github.com/streetteam/aws-ec2-ssh.git
 
 cd "$tmpdir/aws-ec2-ssh"
 
